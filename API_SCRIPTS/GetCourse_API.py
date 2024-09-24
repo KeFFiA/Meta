772715251c5dd903ad22d5a89f854d30e2223c72
Alexander <80920304+KeFFiA@users.noreply.github.com>
2024-09-25 02:22:19 +0300

    Update reports APIs

--- API_SCRIPTS/GetCourse_API.py
+++ API_SCRIPTS/GetCourse_API.py
@@ -109,6 +109,23 @@ async def getcourse_users_report(account_name, params, data):
             await sleep(60)
             time -= 60
 
+    getcourse_db.query(query="""DELETE FROM getcourse_users
+                WHERE ctid NOT IN (
+                    SELECT MIN(ctid)
+                    FROM getcourse_users
+                    GROUP BY id, email, registration_type, 
+                        created, last_activity, first_name, last_name, phone, date_of_birth, age, country, 
+                        city, from_partner, client_portrait, comments, bil_minut, dosmotrel_do_kontsa, 
+                        button_click, web_room, data_webinara, vremya_start, vremya_end, bil_na_webe, 
+                        banner_click, city_2, pay_at, sb_id, partner_id, partner_email, partner_full_name, 
+                        utm_source, utm_medium, utm_campaign, utm_term, utm_content, bil_minut_2, utm_group, 
+                        btn_1, btn_2, btn_3, lm_utm_source, lm_utm_medium, lm_utm_term, lm_utm_content, 
+                        lm_utm_campaign, btn_4, btn_5, btn_6, instagram_telegram_nick, income_money, btn_7, 
+                        btn_8, web_time, webhook_time_web, btn_9, from_where, utm_source_2, utm_medium_2, 
+                        utm_campaign_2, utm_term_2, utm_content_2, utm_group_2, partner_id_2, partner_email_2, 
+                        partner_fullname, manager_fullname, vk_id
+                );""")
+
 
 async def getcourse_deals_report(account_name, params, data):
     time = 1800
@@ -154,6 +171,23 @@ async def getcourse_deals_report(account_name, params, data):
             await sleep(120)
             time -= 120
 
+    getcourse_db.query(query="""DELETE FROM getcourse_deals
+                        WHERE ctid NOT IN (
+                            SELECT MIN(ctid)
+                            FROM getcourse_deals
+                            GROUP BY Order_ID, Number, User_ID, Username, Email, Phone, Creation_Date, Payment_Date, Title, 
+                            Status, Cost_RUB, Paid, Payment_System_Fee, Received, Tax, 
+                            Remaining_After_Deducting_Payment_System_Fee_and_Tax, Other_Fees, Earned, Currency, Manager, 
+                            City, Payment_System, Partner_ID, Used_Promo_Code, Promotion, Partner_Evgeny_Schneider, 
+                            Code_Word, Club_Opening_Date, Payment_Date_1st_Payment, second_payment_date, third_Payment_Date, 
+                            utm_source, utm_medium, utm_campaign, utm_content, utm_term, Active_Messenger, sb_id, gcpc, 
+                            fun, gcmlg, Order_Creation_Page, number_manager, time_web, date_web, time_left, replay_web, 
+                            Order_Partner_ID, Order_Partner_Email, Order_Partner_Full_Name, User_Partner_ID, User_Partner_Email, 
+                            User_Partner_Full_Name, utm_source_2, utm_medium_2, utm_campaign_2, utm_content_2, utm_term_2, 
+                            utm_group, Affiliate_Source, Affiliate_Code, Affiliate_Session, user_utm_source, user_utm_medium, 
+                            user_utm_campaign, user_utm_content, user_utm_term, user_utm_group, user_gcpc, Tags, Offer_Tags
+                        );""")
+
 
 async def getcourse_payments_report(account_name, params, data):
     time = 1200
@@ -184,5 +218,13 @@ async def getcourse_payments_report(account_name, params, data):
             getcourse_logger.error(msg=f'GetCourse payments_report failed with error: {_ex}')
             await sleep(120)
             time -= 120
+
+    getcourse_db.query(query="""DELETE FROM getcourse_payments
+                            WHERE ctid NOT IN (
+                                SELECT MIN(ctid)
+                                FROM getcourse_payments
+                                GROUP BY Number, Username, Email, Orders, 
+                                         Creation_Date, Type, Status, Amount, Fees, Received, Payment_Code, Title
+                            );""")
 
 
